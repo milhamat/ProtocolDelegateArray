950222f1a8ecb77dfba878d42c063a24e5061116
milhamat <m477ashiddiq@gmail.com>
2021-01-29 15:59:25 +0700

    Initial Commit

---
+++ ProtocolDelegateArray/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolDelegateArray
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 29/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
